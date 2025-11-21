cf136e9cb01ae5fe8a102ab128bb682eb72099b5
Mohammed Salem <mohamedsalem462006@gmail.com>
2025-11-21 06:31:39 +0200

    Refactor Name class with constructors and methods

--- Name.h
+++ Name.h
@@ -1,11 +1,38 @@
 #ifndef NAME_FILE
 #define NAME_FILE
+#include <bits/stdc++.h>
+using namespace std;
 
-// pls uncomment line number 42 in person class when complete " friend ostream& operator<< " method
-
-class Name 
+class Name
 {
+private:
+    string firstName;
+    string lastName;
 
+public:
+    Name(): firstName(""), lastName(""){}
+
+    Name(const string &first, const string &last): firstName(first), lastName(last){}
+
+    void setFirstName(const string &first)
+    {
+        firstName = first;
+    }
+
+    void setLastName(const string &last)
+    {
+        lastName = last;
+    }
+
+    string getFullName() const
+    {
+        return firstName + " " + lastName;
+    }
+
+    friend ostream &operator << (ostream &out, const Name &name)
+    {
+        out << name.firstName << " " << name.lastName;
+        return out;
+    }
 };
-
 #endif
