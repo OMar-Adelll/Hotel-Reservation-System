49cccbcffcc381a42e0ac01e052e697ca4b8f9c9
Omar Adel <OmarAdel.connect@gmail.com>
2025-11-25 10:36:23 +0200

    Add new features in Hotel Class

--- Hotel.h
+++ Hotel.h
@@ -12,7 +12,8 @@ class Hotel : public ID
     int roomsCount;
     int customersCount;
     string description;
-    map<RoomType, vector<Room1 *>> roomsByType;
+    map<RoomType, map<int, Room1 *>> Rooms; // (roomtype , (Id , room1))
+    map<int, Room1 *> RoomsByNumber;        // (roomNumber , room1)
 
 public:
     Hotel() : ID(Object::Hotel)
@@ -47,40 +48,113 @@ public:
     void setCustomersCount(int count) { customersCount = count; }
     void setDescription(const string &desc) { description = desc; }
 
-    void build_Room(int roomNumber, RoomType t)
+    void build_Room(int roomNumber, RoomType t) // roomNumber required because it's essential thing when you intialize new room it (room number, type)
     {
         Room1 *r = new Room1(roomNumber, t);
-        roomsByType[t].push_back(r);
+        const int id = r->getID();
+        Rooms[t][id] = r;
+        RoomsByNumber[roomNumber] = r;
         roomsCount++;
     }
 
     void addRoom(const Room1 &other)
     {
         Room1 *r = new Room1(other.getRoomNumber(), other.getRoomType());
-        roomsByType[other.getRoomType()].push_back(r);
+        const int id = r->getID();
+        Rooms[other.getRoomType()][id] = r;
+        RoomsByNumber[other.getRoomNumber()] = r;
         roomsCount++;
     }
 
-    bool foundRoom()
+    bool foundRoom(Room1 other) // search with Room
     {
-        // TODO
+        RoomType type = other.getRoomType();
+        const int id = other.getID();
+        auto it = Rooms[type].find(id);
+        if (it != Rooms[type].end())
+            return true;
+        return false;
     }
 
-    void deleteRoom()
+    bool foundRoom(int roomNumber) // search with Room number
     {
-        // TODO
+        auto it = RoomsByNumber.find(roomNumber);
+        if (it != RoomsByNumber.end())
+            return true;
+        return false;
+    }
+
+    void deleteRoom(Room1 other)
+    {
+        if (!foundRoom(other))
+            return void(cout << "This Room is Not Found !" << nl);
+        RoomType type = other.getRoomType();
+        const int id = other.getID();
+        int roomNumber = other.getRoomNumber();
+        Rooms[type].erase(id);
+        RoomsByNumber.erase(roomNumber);
+        roomsCount--;
+    }
+
+    void deleteRoom(int roomNumber) // delete with room number
+    {
+        if (!foundRoom(roomNumber))
+            return void(cout << "This Room isn't Found !" << nl);
+        Room1 *r = RoomsByNumber[roomNumber];
+        RoomType type = r->getRoomType();
+        const int id = r->getID();
+        Rooms[type].erase(id);
+        RoomsByNumber.erase(roomNumber);
+        roomsCount--;
     }
 
     void displayRoomsList()
     {
-        // TODO
+        if (roomsCount == 0)
+        {
+            cout << "No rooms available in this hotel.\n";
+            return;
+        }
+
+        cout << left << setw(10) << "ID"
+             << setw(12) << "Number"
+             << setw(12) << "Type"
+             << setw(12) << "Status" << "\n";
+
+        cout << string(46, '-') << "\n";
+
+        for (auto &typeGroup : Rooms)
+        {
+            for (auto &roomPair : typeGroup.second)
+            {
+                Room1 *room = roomPair.second;
+
+                cout << left
+                     << setw(10) << room->getID()
+                     << setw(12) << room->getRoomNumber()
+                     << setw(12) << room->getRoomType()
+                     << setw(12) << room->getRoomStatus()
+                     << "\n";
+            }
+        }
+    }
+    Room1 *findRoomByNumber(int roomNumber)
+    {
+        auto it = RoomsByNumber.find(roomNumber);
+        if (it != RoomsByNumber.end())
+            return it->second;
+        return nullptr;
     }
 
     ~Hotel()
     {
-        for (auto &free : roomsByType)
-            for (auto &room : free.second)
-                delete room;
+        for (auto &typeGroup : Rooms)
+        {
+            for (auto &roomPair : typeGroup.second)
+                delete roomPair.second;
+            typeGroup.second.clear();
+        }
+        Rooms.clear();
     }
 };
 
